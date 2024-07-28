b1e6d27b560052a83aca04ae4b0a700995b59cb4
ScottSucksAtProgramming <scottkostolni+github@gmail.com>
2024-07-28 13:55:12 -0400

    adjusted prompts for clarity.

--- modules/prompts/narrative_prompts.py
+++ modules/prompts/narrative_prompts.py
@@ -194,22 +194,10 @@ narrative_prompts = {
 
             Lab Values Sub-Section
             This section contains the results of any available laboratory values, and Point of Care Testing. As in the previous sections this should be presented as a bulleted list with short and concise sentences, or values. Any items which have no information should be omitted from the narrative. Lab values may include: Metabolic, Hematology, Hepatic, Coagulation, Cardiac, Other (for any values not in the other categories), Infectious, and Arterial Blood Gases. Each Category is follow by the specific lab test, it's resulted value and where it falls within the range; (H) high, (N) normal, (L) low. if there are no lab results provided they can be omitted. Don't leave them empty. Following the lab values are point of care testing results, these include the patient's Vital Signs (Blood Pressure, Hear Rate (rhythm and quality), Respiration (rate and quality) SpO2 reading (on room air, or oxygen), The patient's EKG interpretation, the patient's blood glucose level, the patient's pain score, and the patients End Tidal CO2 (EtCO2) result. As before if any of this information is not provided you can omit it from the narrative. If no information applicable to this subsection is provided you can omit the entire section.
-
-            Abbreviations used:
-            - Na- Sodium, K- Potassium; Cl- Chloride; CO2- Carbon Dioxide (can sometimes be notated as HCO3); BUN- Blood Urea Nitrogen; Cr- Creatinine; Gluc- Glucose; Ca- Calcium; Mg- Magnesium; Phos- Phosphorus 
-            - WBC- White Blood cell Count; Hgb- Hemoglobin; Hct- Hematocrit; Plat- Platelets
-            - Tot. Bilirubin- Total Bilirubin; ALT- Alanine transaminase; AST- Aspartate transaminase; ALP- Alkaline phosphatase;
-            - Pt- Prothrombin time; (a)PTT- Activated partial thromboplastin time; INR- International Normalized Ratio;
-            - CK- creatine kinase; CK-MB- creatine kinase-MB test; CPK- creatine phosphokinase; BNP- brain natriuretic peptide
-            - pH- Power of Hydrogen; PaO2- Partial pressure of arterial oxygen; PaCO2- Partial pressure of arterial carbon dioxide; HCO3- Bicarbonate; SaO2- Saturation of arterial oxygen; 
-            - EKG- electrocardiogram (can also be abbreviated as ECG)
-            - BGL- Blood Glucose Level (should always be measured in mg/dl milligrams per deciliter)
-            - EtCO2- End-Tidal Carbon Dioxide (always measured in mmHg  millimeters of Mercury
-
-
+            
             Rules for this Section
             - Do not make any assumptions or inferences. Stick to documenting only the information is provided. If you are unsure, ask the user for additional information or how they would like the information they provided to be written out.
-            - If any key information seems to be missing or unclear in a section, prompt the user to provide additional details.
+            - Never add any values which are not included in the provided data.
             - Not all laboratory tests or point of care tests will be used on every patient. Skip them if the data does not provide them. 
             - Do not provide and additional information, context, or explanations. Only provide the completed section in the proper format.
 
@@ -229,9 +217,6 @@ narrative_prompts = {
                 - BGL: 98 mg/dl;
                 - Pain: 8/10;
                 - eTCO2: 37 mmHg;
-
-            Other Formatting:
-            For lab values: The abbreviation for the test should always be followed by a colon (:) a space and then the value. The value should be followed by its location in the range surrounded by parenthesis. Each result should be ended with a semi-colon (;) and a space before moving on to the next one. Ensure that the formatting, such as the use of colons (:) and semicolons (;), is consistent throughout the bulleted lists to maintain clarity and readability.
         """
     }
 }   
