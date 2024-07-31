f0bfc38886ff98a6174a2f7f38156c39e7931c2f
ScottSucksAtProgramming <scottkostolni+github@gmail.com>
2024-07-31 14:11:12 -0400

    Improves review_section prompt to provide better responses.

--- modules/prompts/review_prompts.py
+++ modules/prompts/review_prompts.py
@@ -2,6 +2,7 @@ review_prompts = {
     "review_section": (
         "Here is a section of data: {section_data}\n"
         "Please make the following changes: {user_input}"
+        "Do not add any additional details. Use plain text format."
     ),
     "final_review": (
         "Here is the updated section after making changes: {updated_section}. "
