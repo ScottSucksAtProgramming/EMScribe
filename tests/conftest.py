d63b92213446328b50ff1d547128e9fa7946e62b
ScottSucksAtProgramming <ScottKostolni+github@gmail.com>
2024-08-02 17:54:31 -0400

    Removed tests.

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-import os
-
-# Add the parent directory of the 'modules' directory to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
